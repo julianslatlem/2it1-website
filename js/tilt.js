6d253cf864d23f26e84ce43280aefdf6a5eda868
Julian Slatlem <79726826+julianslatlem@users.noreply.github.com>
2024-09-03 11:33:34 +0200

    Contact page

---
+++ js/tilt.js
@@ -0,0 +1,10 @@
+VanillaTilt.init(document.querySelectorAll(".person"), {
+    max: 10,
+    speed: 1200,
+    glare: true,
+    "max-glare": 0.2,
+
+    transition: true,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+    scale: 1.1,
+});
